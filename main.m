a7dbbfe5a87ffbda995ab532c2d4800b2deaea48
Mike Ash <mike@mikeash.com>
2010-12-01 22:10:32 -0500

    add quickie proof-of-concept test

--- main.m
+++ main.m
@@ -1,9 +1,26 @@
 #import <Foundation/Foundation.h>
 
+#import "MAAsyncReader.h"
+
+
 int main (int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    NSLog(@"Hello, World!");
+    
+    int fd = open("/dev/null", O_RDONLY);
+    MAAsyncReader *reader = [[MAAsyncReader alloc] initWithFileDescriptor: fd];
+    [reader setErrorHandler: ^(int err) {
+        NSLog(@"got error %d (%s)", err, strerror(err));
+    }];
+    
+    [reader readUntilCondition: ^NSUInteger (NSData *buffer) { return 0; }
+                      callback: ^(NSData *data) {
+                          NSLog(@"got data %@", data);
+                          if(!data)
+                              exit(0);
+                      }];
+    
+    dispatch_main();
     [pool drain];
     return 0;
 }
